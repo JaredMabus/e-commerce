26226efad64a8c37fb0fc46e28b61276eb224869
JaredMabus <65479004+JaredMabus@users.noreply.github.com>
2022-10-24 19:49:12 -0500

    added CRUD routes for ecommerce_db

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    let data = await Product.findAll({
+      include: [{ model: Tag }]
+    });
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    let data = await Product.findByPk(req.params.id, {
+      include: [{ model: Tag }]
+    });
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -89,8 +109,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    let data = await Product.destroy({ where: { id: req.params.id } });
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
 module.exports = router;
